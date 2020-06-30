a624b738808b2711b4ae59a6380a32f7f707e509
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-30 23:57:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,8 +19,8 @@ def using_flatten(instruments)
   
 end
 
-def using_delete(instructors,)
-  instructors.delete[1]
+def using_delete(instructors,no_offense_steven)
+  instructors.delete(no_offense_steven)
   
 end
 
