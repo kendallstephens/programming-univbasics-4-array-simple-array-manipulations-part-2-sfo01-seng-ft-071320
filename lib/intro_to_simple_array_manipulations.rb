aef05c351ebcb5d5f3abed153874c618433795a4
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-30 23:43:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,9 @@ def using_concat(my_favorite_things, more_favs)
   
 end
 
-def using_insert(list_of_programming_arrays, )
+def using_insert(list_of_programming_arrays, another_language)
+  
+end
 
 
 
